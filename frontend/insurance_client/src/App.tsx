1ac951648da74ec176b2da1c7b4c61bbba435262
Caroline Lewis <47carolines@gmail.com>
2026-03-01 17:32:17 -0600

    basic login setup

--- frontend/insurance_client/src/App.tsx
+++ frontend/insurance_client/src/App.tsx
@@ -3,8 +3,16 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Welcome to the Insurance Quote App</h1>
-    </>
+        <h2>Login</h2>
+
+      <input placeholder="Email" />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+
+      <button>Login</button>
+      </>
   )
 }
 
